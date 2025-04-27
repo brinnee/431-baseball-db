ee9f9faccce0b2ccac56628e62eeedb1a7cda21d
Ismail Peracha <91555891+ismailperachaprogramming@users.noreply.github.com>
2025-04-26 19:16:10 -0700

    Update homepage.php

--- homepage.php
+++ homepage.php
@@ -1,7 +1,38 @@
+<?php
+session_start();
+
+if (!isset($_SESSION['role'])) {
+    header('Location: login.php');
+    exit();
+}
+
+$role = $_SESSION['role'];
+?>
+
 <!DOCTYPE html>
 <html>
-  <head>
-    <title>CPSC 431 final project</title>
-  </head>
-  <body>
-    <h1 style="text-align:center">Zoo</h1>
+<head>
+    <title>Zoo Homepage</title>
+</head>
+<body>
+    <h1>Welcome to the Zoo Management System</h1>
+
+    <?php if ($role == 'owner') { ?>
+        <a href="owner_page.php">Owner Functions</a><br>
+    <?php } ?>
+
+    <?php if ($role == 'manager') { ?>
+        <a href="manager_page.php">Manager Functions</a><br>
+    <?php } ?>
+
+    <?php if ($role == 'employee') { ?>
+        <a href="employee_page.php">Employee Functions</a><br>
+    <?php } ?>
+
+    <?php if ($role == 'customer') { ?>
+        <a href="customer_page.php">Customer Functions</a><br>
+    <?php } ?>
+
+    <a href="logout.php">Logout</a>
+</body>
+</html>
