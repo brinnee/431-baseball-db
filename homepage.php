36ba282196c33a2490e49be19c02d5826ce1c625
britney c. <britneyc2502@csu.fullerton.edu>
2025-05-01 00:37:13 -0700

    login for coach

--- homepage.php
+++ homepage.php
@@ -22,7 +22,7 @@ $role = $_SESSION['role'];
     <?php } ?>
 
     <?php if ($role == 'coach') { ?>
-        <a href="coach_page.php">Coach Functions</a><br>
+        <a href="viewteam.php">Coach Functions</a><br>
     <?php } ?>
 
     <?php if ($role == 'player') { ?>
